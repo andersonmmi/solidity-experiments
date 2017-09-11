6a5ab2bc9ca92cb04cb202ce92ede14603e25f82
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-10 23:23:47 -0400

    ProofOfExistence1 notarize(), calculateProof(), and proof() execute successfully

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const ConvertLib = artifacts.require("./ConvertLib.sol");
+const MetaCoin = artifacts.require("./MetaCoin.sol");
+const ProofOfExistence1 = artifacts.require('./ProofOfExistence1');
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.deploy(ProofOfExistence1);
 };
